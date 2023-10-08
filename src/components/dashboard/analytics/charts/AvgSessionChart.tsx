8bcfecbfcad0a5aeebe386e4532bb12c51473c04
Djibh <julien.riera@protonmail.com>
2023-10-08 17:36:19 +0200

    style (F03:AVGCHART) > Update chart layout

--- src/components/dashboard/analytics/charts/AvgSessionChart.tsx
+++ src/components/dashboard/analytics/charts/AvgSessionChart.tsx
@@ -1,26 +1,44 @@
-import { LineChart, Line, XAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer, YAxis } from 'recharts';
 import { theme } from '../../../../theme';
+import styled from 'styled-components';
 
 type AvgSessionChartProps = {
   data: object[]
 }
 
 export default function AvgSessionChart({ data }: AvgSessionChartProps) {
-  return (
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart style={{backgroundColor: `${theme.colors.lineChartRed}`, borderRadius: `${theme.borderRadius.medium}`}} 
-           data={data}>
-          <XAxis dataKey="day"/>
-          <Tooltip />
-          <Legend />
-          <defs>
-            <linearGradient id="paint0_linear_2_122" x1="309.906" y1="-1.97779" x2="-47.7754" y2="-1.97779" gradientUnits="userSpaceOnUse">
-              <stop stopColor="white"/>
-              <stop offset="0.810441" stopColor="white" stopOpacity="0.403191"/>
-            </linearGradient>
-          </defs>
-          <Line type="basis" dot={false} activeDot={true} dataKey="sessionLength" stroke="url(#paint0_linear_2_122)" strokeWidth={2}/>
-        </LineChart>
-      </ResponsiveContainer>
+  return (<>
+        <AvgSessionsTitleStyled>
+          Durée moyenne des sessions
+        </AvgSessionsTitleStyled>
+        <ResponsiveContainer width="100%" height="100%" >
+            <LineChart 
+                margin={{ top: 70, right: 30, left: 20, bottom: 40 }}
+                style={{backgroundColor: `${theme.colors.lineChartRed}`, borderRadius: `${theme.borderRadius.medium}`}} 
+                data={data}>
+              <XAxis dataKey="day" axisLine={false} tickLine={false} tick={{fill: "white", opacity: .6, fontSize: "0.9rem"}} tickMargin={20} />
+              <YAxis hide domain={['dataMin-10', 'dataMax+10']} />
+              <Tooltip />
+              <defs>
+                <linearGradient id="line-gradient" x1="309.906" y1="-1.97779" x2="-47.7754" y2="-1.97779" gradientUnits="userSpaceOnUse">
+                  <stop stopColor="white"/>
+                  <stop offset="0.810441" stopColor="white" stopOpacity="0.403191"/>
+                </linearGradient>
+              </defs>
+              <Line type="natural" dot={false} activeDot={{stroke: '#FFF', strokeWidth: 4, r: 2	}} dataKey="sessionLength" stroke="url(#line-gradient)" strokeWidth={2}/>
+            </LineChart>
+        </ResponsiveContainer>
+      </>
   )
 }
+
+const AvgSessionsTitleStyled = styled.h1`
+  position: absolute;
+  margin: 0;
+  padding: 18px;
+  color: #20253a;
+  opacity: 0.6;
+  font-size: .95rem;
+  font-weight: 500;
+  z-index: 5;
+  `;
