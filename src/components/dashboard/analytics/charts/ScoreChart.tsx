47f9b3c35da762ef9e94c48e3bd1bf19a54a0716
Djibh <julien.riera@protonmail.com>
2023-10-13 11:17:13 +0200

    feat (F03:SCORE) > Finalize bar to score adjustment value

--- src/components/dashboard/analytics/charts/ScoreChart.tsx
+++ src/components/dashboard/analytics/charts/ScoreChart.tsx
@@ -3,33 +3,33 @@ import { theme } from '../../../../theme';
 import styled from 'styled-components';
 
 type ScoreChartProps = {
-  score?: number,
-  className?: string
+  score: number
 }
 
 export default function ScoreChart({ score }: ScoreChartProps) {
   let scoreToPercent; 
   if (score) {
-    scoreToPercent = score * 100
+    scoreToPercent = Math.round(score * 100)
   }
   
-  const data = [{ value: score }]
+  const data = [{ value: 1 }, { value: score }]
   
   return ( <>
         <ResponsiveContainer width="100%" height="100%">
             <RadialBarChart 
-              innerRadius="65%" 
-              outerRadius="80%" 
+              innerRadius="55%" 
+              outerRadius="75%" 
               data={data} 
               startAngle={90} 
-              endAngle={200}
+              endAngle={ 450 }
+              barSize={10}
               style={{backgroundColor: `${theme.colors.analyticsBackground}`, borderRadius: `${theme.borderRadius.medium}`}}
               >
               <RadialBar dataKey='value' cornerRadius={16} fill={`${theme.colors.accentRed}`}/>
             </RadialBarChart>
         </ResponsiveContainer>
 
-        <span style={{position: "absolute", top: "10px", left: "10px", color: `${theme.colors.chartsTitleDark}`}}>Score</span>
+        <span style={{position: "absolute", top: "15px", left: "15px", color: `${theme.colors.chartsTitleDark}`}}>Score</span>
         <ScoreContentStyled>
           <span>{ scoreToPercent }%</span>
           <span>de votre objectif</span>
@@ -46,13 +46,14 @@ const ScoreContentStyled = styled.div`
     align-items: center;
     top: 50%;
     left: 50%;
-    width: 56%;
-    height: 56%;
+    width: 58%;
+    height: 58%;
     margin: 0;
     text-align: center;
     transform: translate(-50%, -50%);
 
     border-radius: 50%;
+    border: 1px solid white;
     background-color: white;
     color: ${theme.colors.chartsTitleDark};
     font-weight: 500;
@@ -63,12 +64,12 @@ const ScoreContentStyled = styled.div`
     }
 
     & * {
-      font-size: 0.65rem;
+      font-size: 0.85rem;
       color: ${theme.colors.activityChartLegend};
     }
 
     & :first-child {
-      font-size: 1.3rem;
+      font-size: 1.7rem;
       color: ${theme.colors.chartsBlack};
     }
 `;
